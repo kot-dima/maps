2ce2931611bc329a0fd120d54adb0d715f0a13c1
Дмитро Кот <Kot@rivne.itstep.org>
2019-12-19 21:01:16 +0200

    first commit

---
+++ intro/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import React from 'terminal/terminal'
+import React from 'search/search'
+import React from 'map/map'
+
+ReactDOM.render(<App />, document.getElementById('root'));
